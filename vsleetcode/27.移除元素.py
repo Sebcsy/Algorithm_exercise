d5a535e91a57149e1a19988a8a11d2dae4bcdb7a
Sebcsy <374869061@qq.com>
2022-12-05 11:07:49 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'master/master'.
    
    Changes to be committed:
    	new file:   "vsleetcode/27.\347\247\273\351\231\244\345\205\203\347\264\240.py"

---
+++ vsleetcode/27.移除元素.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode.cn id=27 lang=python
+#
+# [27] 移除元素
+#
+
+# @lc code=start
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        slow=0
+        fast=0
+        for fast in range(len(nums)):
+            if nums[fast]!=val:
+                nums[slow]=nums[fast]
+                slow+=1
+        return slow
+# @lc code=end
+
